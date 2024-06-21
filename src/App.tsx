d6dbaff005998e5cc05dda76871730ec7538372a
Yur3te <Yur3te@gmail.com>
2024-06-21 18:51:40 +0200

    responsivity stuff, scrolling is repaired on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Panel } from './components/Panel/Panel';
 
 const App: React.FC = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-neutral-950 font-inter">
+    <div className="flex items-center justify-center min-h-[100dvh] sm:min-h-screen bg-neutral-950 font-inter">
       <Panel />
     </div>
   );
